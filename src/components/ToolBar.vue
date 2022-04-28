affc65f482fa6cd8af2060988aa7ca09e9730e30
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-29 01:32:21 +0900

    ToolBar의 라우터 링크 스타일링

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-        <router-link to="/news">News</router-link>
-        <router-link to="/ask">Ask</router-link>
+    <div class="header">
+        <router-link to="/news">News</router-link> |
+        <router-link to="/ask">Ask</router-link> |
         <router-link to="/jobs">Jobs</router-link>
     </div>
 </template>
 
 <script>
 export default {
+
 };
 </script>
 
 <style lang="scss" scoped>
+.header {
+    color: #fff;
+    background-color: #42b883;
+    display: flex;
+    padding: 8px;
+    
+    .router-link-exact-active {
+        color: #35495e;
+    }
 
+    a {
+        color: #fff;
+    }
+
+}
 </style>
