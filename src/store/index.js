f1d13f91ee4672a0a111094a3833f53b230626a0
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-30 02:22:26 +0900

    NewsView에 actions와 mutations 적용

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchNewsList } from '@/api/index.js'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
         news: [],
+    },
+    mutations: {
+        SET_NEWS(state, news) { // state라는 인자로 state에 접근 가능, actions에서 response.data로 넘긴 데이터를 news로 받음
+            state.news = news;
+        }
+    },
+    actions: {
+        FETCH_NEWS(context) { // context라는 인자는 mutations에 접근 할 수 있게 해줌
+            fetchNewsList()
+            .then(response => {
+                console.log(response.data);
+                context.commit('SET_NEWS', response.data);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
     },
 });
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,31 +1,20 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user.id">{{ user.title }}</div>
+    <div v-for="user in this.$store.state.news" :key="user.id">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '@/api/index.js'
 
 export default {
-  data() {
-    return {
-      users: [],
-    }
-  },
   created() {
-    console.log('호출 전: ', this); // 호출 전 VueComponent
-    fetchNewsList()
-    // .then(function(response) {
+    this.$store.dispatch('FETCH_NEWS');
+    // fetchNewsList()
+    // .then(response => {
     //   console.log(response.data);
-    //   console.log('호출 후: ', this); // 호출 후 undefined
+    //   this.users = response.data; 
     // })
-    .then(response => {
-      console.log(response.data);
-      console.log('호출 후: ', this); // 호출 후 VueComponent
-      this.users = response.data; 
-    })
-    .catch(error => console.log(error))
+    // .catch(error => console.log(error))
   }
 };
 </script>
