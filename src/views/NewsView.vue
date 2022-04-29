380ade3516f7e36d2ca98e7ded01f671a0b79090
ellin-hand <ellin.hand@kakaocorp.com>
2022-04-30 01:02:23 +0900

    자바스크립트 this 4가지와 화살표 함수의 this

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -14,9 +14,15 @@ export default {
     }
   },
   created() {
+    console.log('호출 전: ', this); // 호출 전 VueComponent
     fetchNewsList()
+    // .then(function(response) {
+    //   console.log(response.data);
+    //   console.log('호출 후: ', this); // 호출 후 undefined
+    // })
     .then(response => {
       console.log(response.data);
+      console.log('호출 후: ', this); // 호출 후 VueComponent
       this.users = response.data; 
     })
     .catch(error => console.log(error))
