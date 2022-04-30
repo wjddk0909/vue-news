4687eb2d5bf41f3be1a687a7ec989bf34bbb80ca
wjddk0909 <fjddkcb@naver.com>
2022-05-01 03:07:33 +0900

    라우터 트랜지션

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -17,4 +19,15 @@ export default {
 
 <style lang="scss">
 @import '@/assets/scss/common.scss';
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
