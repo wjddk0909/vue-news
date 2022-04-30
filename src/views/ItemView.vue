962f263adf8b32e755d7701d1a6635e76f7a9862
wjddk0909 <fjddkcb@naver.com>
2022-05-01 02:59:10 +0900

    질문 상세 페이지 스타일링 및 v-html 디렉티브 사용법 소개

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
-        <p>{{ itemInfo.title }}</p>
-        <p>{{ itemInfo.content }}</p>
+        <section>
+            <!-- 질문 상세 정보 -->
+            <div class="user-container">
+                <div>
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="user-description">
+                    <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
+                    <div class="time">{{ fetchedItem.time_ago }}</div>
+                </div>
+            </div>
+            <h2>{{ fetchedItem.title }}</h2>
+        </section>
+        <section>
+            <!-- 질문 댓글 -->
+            <div v-html="fetchedItem.content"></div>
+        </section>
+        <!-- <p>{{ fetchedItem.title }}</p>
+        <p>{{ fetchedItem.content }}</p> -->
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     computed: {
-        itemInfo() {
-            return this.$store.state.item;
-        }
+        // itemInfo() {
+        //     return this.$store.state.item;
+        // }
+        ...mapGetters(['fetchedItem'])
     },
     created() {
         const itemId = this.$route.params.id
@@ -20,5 +40,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
 
+    .fa-user {
+        font-size: 2.5rem;
+    }
+
+    .user-description {
+        padding-left: 8px;
+    }
+
+    .time {
+        font-size: 0.7rem;
+    }
+}
 </style>
