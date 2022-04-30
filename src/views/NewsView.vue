320803e3759da42445e15c713f151a50e2fb46e0
wjddk0909 <fjddkcb@naver.com>
2022-04-30 21:49:17 +0900

    동적 라우트 매칭 원리 및 적용

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -2,7 +2,10 @@
   <div>
     <p v-for="item in this.$store.state.news" :key="item.id">
       <a v-bind:href="item.url">{{ item.title }}</a>
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
+      <small>
+        {{ item.time_ago }} 
+        by <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+        </small>
     </p>
   </div>
 </template>
