998719e87d71186f1059ae6b391e714f202d703a
wjddk0909 <fjddkcb@naver.com>
2022-05-02 02:35:41 +0900

    공통 컴포넌트 ListItem 제작

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,68 +1,19 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li v-for="item in this.$store.state.news" :key="item.id" class="post">
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <div>
-          <p class="news-title">
-            <a v-bind:href="item.url">
-              {{ item.title }}
-            </a>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by <router-link :to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
-          </small>
-        </div>
-        <!-- <a v-bind:href="item.url">
-          {{ item.title }}
-        </a>
-        <small>
-          {{ item.time_ago }} 
-          by <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-        </small> -->
-      </li>
-    </ul>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
+import ListItem from '@/components/ListItem.vue'
 
 export default {
-  created() {
-    this.$store.dispatch('FETCH_NEWS');
+  components: {
+    ListItem,
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
 
-  .post {
-    list-style: none;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eee;
-
-    .points {
-      width: 80px;
-      height: 60px;
-      display: flex;
-      align-items: center; // (flex)세로 중앙 정렬
-      justify-content: center; // (flex)가로 중앙 정렬
-      color: #42b883
-    }
-
-    .news-title {
-      margin: 0;
-    }
-
-    .link-text {
-      color: #828282
-    }
-  }
-}
 </style>
