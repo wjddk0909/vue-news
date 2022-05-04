14deeeb5aea1f060678ff92447969656daec4c08
wjddk0909 <fjddkcb@naver.com>
2022-05-04 16:28:49 +0900

    사용자 프로필 컴포넌트 소개 및 등록

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="user-container">
+                <div>
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="user-description">
+                    <!-- <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
+                    <div class="time">{{ fetchedItem.time_ago }}</div> -->
+                </div>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss" scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+
+    .fa-user {
+        font-size: 2.5rem;
+    }
+
+    .user-description {
+        padding-left: 8px;
+    }
+
+    .time {
+        font-size: 0.7rem;
+    }
+}
+</style>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <p>name: {{ userInfo.id }}</p>
+        <user-profile></user-profile>
+        <!-- <p>name: {{ userInfo.id }}</p>
         <p>karma: {{ userInfo.karma }}</p>
-        <p>crated: {{ userInfo.created }}</p>
+        <p>crated: {{ userInfo.created }}</p> -->
     </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import UserProfile from '@/components/UserProfile.vue'
 
 export default {
+    components: {
+        UserProfile
+    },
     computed: {
         userInfo() {
             return this.$store.state.user;
