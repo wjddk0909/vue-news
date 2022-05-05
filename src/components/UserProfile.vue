aa25c1db22d662283737d105c1b3a0dbf119e5b8
wjddk0909 <fjddkcb@naver.com>
2022-05-05 16:47:39 +0900

    slot을 이용한 사용자 프로필 컴포넌트 구현

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -5,12 +5,21 @@
                     <i class="fas fa-user"></i>
                 </div>
                 <div class="user-description">
-                    <div>{{ info.id }}</div>
+                    <slot name="username">
+                        <!-- 상위 컴포넌트에서 정의할 영역 -->
+                    </slot>
+                    <!-- <div>{{ info.id }}</div> -->
                     <!-- <router-link :to="`/user/${userInfo.user}`">
                         {{ userInfo.id }}
                     </router-link> -->
                     <div class="time">
-                        {{ info.created }}
+                        <slot name="time">
+                            <!-- 상위 컴포넌트에서 정의할 영역 -->
+                        </slot>
+                        <!-- {{ info.created }} -->
+                    </div>
+                    <div>
+                        <slot name="karma"></slot>
                     </div>
                 </div>
             </div>
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
         <section>
-            <!-- 질문 상세 정보 -->
-            <div class="user-container">
+            <!-- 사용자 정보 -->
+            <user-profile :info="fetchedItem">
+                <div slot="username">{{ fetchedItem.user }}</div>
+                <div slot="time">{{ fetchedItem.time_ago }}</div>
+            </user-profile>
+            <!-- <div class="user-container">
                 <div>
                     <i class="fas fa-user"></i>
                 </div>
@@ -10,7 +14,9 @@
                     <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
                     <div class="time">{{ fetchedItem.time_ago }}</div>
                 </div>
-            </div>
+            </div> -->
+        </section>
+        <section>
             <h2>{{ fetchedItem.title }}</h2>
         </section>
         <section>
@@ -23,9 +29,13 @@
 </template>
 
 <script>
+import UserProfile from '@/components/UserProfile.vue'
 import { mapGetters } from 'vuex'
 
 export default {
+    components: {
+        UserProfile,
+    },
     computed: {
         // itemInfo() {
         //     return this.$store.state.item;
