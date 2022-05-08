a58c83719b1b8befd5ef9f213e964e3135aa219f
wjddk0909 <fjddkcb@naver.com>
2022-05-08 14:24:23 +0900

    하이 오더 컴포넌트(HOC) 소개 및 구현

---
+++ src/views/CreateListView.js
@@ -0,0 +1,11 @@
+import ListView from './ListView.vue'
+
+export default function createListView(name) {
+    return {
+        // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
+        name: name,
+        render(createElement) {
+            return createElement(ListView);
+        }
+    }    
+}
