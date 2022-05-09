0bfe7e56eb6c5acc7258b0534f07ecaa41e3f639
wjddk0909 <fjddkcb@naver.com>
2022-05-09 22:04:02 +0900

    Mixin 적용 후 HOC 구조와 비교

---
+++ src/views/JobsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <list-item></list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from '@/components/ListItem.vue'
+// import bus from '@/utils/bus.js'
+import ListMixin from '../mixins/ListMixin.js'
+
+export default {
+  components: { 
+    ListItem 
+  },
+  mixins: [ListMixin],
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <list-item></list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from '@/components/ListItem.vue'
+// import bus from '@/utils/bus.js'
+import ListMixin from '../mixins/ListMixin.js'
+
+export default {
+  components: {
+    ListItem,
+  },
+  mixins: [ListMixin],
+//   created() {
+//     bus.$emit('start:spinner');
+//     setTimeout(() => {
+//       this.$store.dispatch('FETCH_NEWS')
+//       .then(() => {
+//         console.log('fetched');
+//         bus.$emit('end:spinner');
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       })
+//     }, 3000)
+//   }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
